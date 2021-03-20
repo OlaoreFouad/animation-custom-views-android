2d7d6f2a0ec01d13a6322fec3792e0d7e64ab81f
Olaore Fouad <olaorefouad565@gmail.com>
2021-03-20 15:41:30 +0400

    feature: worked on a custom view that draws arcs based on the value entered into an edittext, was fireeee

--- app/src/main/java/dev/olaore/animationscustomviews/MainActivity.kt
+++ app/src/main/java/dev/olaore/animationscustomviews/MainActivity.kt
@@ -2,10 +2,45 @@ package dev.olaore.animationscustomviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import dev.olaore.animationscustomviews.views.IndicatorView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var indicatorView: IndicatorView
+    private lateinit var colorCountEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        colorCountEditText = findViewById(R.id.color_count)
+        indicatorView = findViewById(R.id.indicator_view)
+
+        colorCountEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                try {
+                    val number = p0.toString().toInt()
+                    if (number > 5 || number < 0) {
+                        Toast.makeText(applicationContext, "Number has to be more between 1 and 5", Toast.LENGTH_LONG).show()
+                    } else {
+                        indicatorView.provideColorCount(number)
+                    }
+                } catch(ex: Exception) {
+                    Log.d("MainActivity", "Something occurred!")
+                }
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
+
     }
 }
---
+++ app/src/main/java/dev/olaore/animationscustomviews/views/IndicatorView.kt
@@ -0,0 +1,65 @@
+package dev.olaore.animationscustomviews.views
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+
+class IndicatorView @JvmOverloads
+    constructor(
+            private val ctx: Context,
+            private val attributeSet: AttributeSet? = null,
+            private val defStyleAttr: Int = 0,
+            private val defStyleRes: Int = 0
+    )
+    : View(ctx, attributeSet, defStyleAttr, defStyleRes) {
+
+    private var colorCount = 1
+    private var colors = listOf(Color.BLACK, Color.CYAN, Color.BLUE, Color.GRAY, Color.GREEN)
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        for (i in 0 until colorCount) {
+            Log.d("IndicatorView", "Index: $i, Angle: ${ getAngleForIndex(i) }")
+            drawSingleCardColor(canvas, i)
+        }
+
+
+
+    }
+
+    fun provideColorCount(number: Int) {
+        this.colorCount = number
+        invalidate()
+    }
+
+    private fun drawSingleCardColor(canvas: Canvas?, cardIndex: Int) {
+
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = colors[cardIndex]
+        }
+
+        canvas?.drawArc(
+                0f, 0f, 200f, 200f, getAngleForIndex(cardIndex).toFloat(), (360 / colorCount).toFloat(), true, paint
+        )
+
+    }
+
+    private fun getAngleForIndex(cardIndex: Int): Int {
+        if (cardIndex == 0) {
+            return -90
+        }
+
+        val sweepAngle = 360 / colorCount
+        var finalAngle = 0
+
+        finalAngle = (cardIndex * sweepAngle) - 90
+        return finalAngle
+
+    }
+
+}
